1f9abc661db619c9611998fb9e3819e581860498
Santhosh <santikumr065@gmail.com>
2025-01-22 17:07:03 +0530

    Update ReservationList.jsx

--- src/pages/ReservationList.jsx
+++ src/pages/ReservationList.jsx
@@ -17,7 +17,7 @@ const ReservationList = () => {
   const getReservationList = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3005/users/${userId}/reservations`,
+        `https://nestcove-be.onrender.com/users/${userId}/reservations`,
         {
           method: "GET",
         }
